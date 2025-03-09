dbb9c64454a7ed675b5efc5f8acac833d2fbfafd
cristinaandreea18 <cristinaandreea577@gmail.com>
2025-03-09 04:47:53 +0200

    seminar 2 updated

--- Seminar2/src/ro/cts/clase/Program.java
+++ Seminar2/src/ro/cts/clase/Program.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Program {
 
 	public static void main(String[] args) {
+
 		List<Aplicant> listaAplicanti;
 		AplicantReader aplicantReader = new AngajatiReader();
 		try {
